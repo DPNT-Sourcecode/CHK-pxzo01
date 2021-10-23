9b1f29406e206546d6a2e5cc4e1d483ae46aa831
sbx_user1051 <sbx_user1051@169.254.0.181>
2021-10-23 10:29:50 +0000

    Sat Oct 23 10:29:50 UTC 2021

--- test/solution_tests/CHK/test_chk.py
+++ test/solution_tests/CHK/test_chk.py
@@ -5,11 +5,13 @@ from solutions.CHK import checkout_solution
     ("AAA", 130),
     ("BB" , 45),
     ("AAbc", -1),
+    ("AAAHN", -1),
     ("aabbcdd12", -1),
     ("762354", -1)
 ])
 
 def test_calculation(skus, total):
     assert checkout_solution.checkout(skus) == total
+
 
 
