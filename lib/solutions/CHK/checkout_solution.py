0ac3afd33754de166143f6e60e3f42d17b794d90
sbx_user1051 <sbx_user1051@169.254.0.181>
2021-10-23 09:37:36 +0000

    Sat Oct 23 09:37:36 UTC 2021

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -7,15 +7,22 @@ def checkout(skus):
     skus = unicode string \n
     special offers: 3A for 130, 2B for 45 
     """
+
+    total_basket_value=0
     basket = Counter(skus.upper())
 
-    for item in basket:
-        print(item)
 
-    return basket
+    for item, cnt in basket.items():
+        item_price = inventory.get(item, -1)
+        print(item, ":", cnt)
+        total_basket_value+=item_price
+        
+
+    return total_basket_value
 
 
 print(checkout("abcfev"))
     
+
 
 
