9fff88c4c46fa30e9a8eb29fe881d11f23d913c3
sbx_user1051 <sbx_user1051@169.254.0.181>
2021-10-23 10:10:36 +0000

    Sat Oct 23 10:10:36 UTC 2021

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -2,15 +2,46 @@ from collections import Counter
 
 inventory = {"A":50, "B":30, "C":20, "D":15}
 
+
+def basket_calculator(goods_basket):
+    basket_value=0
+    for item, cnt in goods_basket.items():
+        # if item not in invenory return -1
+        item_price = inventory.get(item, -1)
+        if item_price==-1:
+            cnt=1
+        # update total basket value
+        # add special offers calculation
+        if item == "A" and cnt>=3:
+            remainder = (cnt % 3) * item_price
+            special_cnt = (cnt - (cnt % 3)) / 3
+
+            special_offer_value = (special_cnt*130) + remainder
+            basket_value+=special_offer_value
+
+        elif item == "B" and cnt>=2:
+            remainder = (cnt % 2) * item_price
+            special_cnt = (cnt - (cnt % 2)) / 2
+
+            special_offer_value = (special_cnt*45) + remainder
+            basket_value+=special_offer_value
+
+        else:
+            basket_value+=(item_price*cnt)
+
+    return int(basket_value)
+
+
 def checkout(skus):
     """
     skus = unicode string \n
     special offers: 3A for 130, 2B for 45
     """
-    total_basket_value=0
+    #total_basket_value=0
     # Count items in basket
     basket = Counter(skus.upper())
 
+    """
     for item, cnt in basket.items():
         # if item not in invenory return -1
         item_price = inventory.get(item, -1)
@@ -34,11 +65,10 @@ def checkout(skus):
 
         else:
             total_basket_value+=(item_price*cnt)
-        
-        
-    return int(total_basket_value)
+    """    
+    total_basket_value = basket_calculator(basket)
+
+    return total_basket_value
     
 
-#print(checkout("aabbcdd12"))
-
-
+print(checkout("aabbcdd12"))
