b556ad2c9ac74d4d0740ca41f907e3582b6774ab
sbx_user1051 <sbx_user1051@169.254.0.181>
2021-10-23 09:31:36 +0000

    Sat Oct 23 09:31:36 UTC 2021

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -1,6 +1,20 @@
+from collections import Counter
 
+inventory = {"A":50, "B":30, "C":20, "D":15}
 
-# noinspection PyUnusedLocal
-# skus = unicode string
 def checkout(skus):
-    raise NotImplementedError()
+    """
+    skus = unicode string \n
+    special offers: 3A for 130, 2B for 45 
+    """
+    if not skus.isalpha():
+        return -1
+    else:
+        basket = Counter(skus.upper())
+        
+    return basket
+
+
+print(checkout("abcfev"))
+    
+
