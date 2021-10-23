d1a24686d2c5609490335fda596a95a3c3d44c8d
sbx_user1051 <sbx_user1051@169.254.0.181>
2021-10-23 10:13:36 +0000

    Sat Oct 23 10:13:36 UTC 2021

--- test/solution_tests/CHK/test_chk.py
+++ test/solution_tests/CHK/test_chk.py
@@ -4,8 +4,10 @@ from solutions.CHK import checkout_solution
 @pytest.mark.parametrize("skus, total",[
     ("aaa", 130),
     ("bb" , 45),
-    ("aabbcdd12", 193)
+    ("aabbcdd12", 193),
+    ("762354", -6)
 ])
 
 def test_calculation(skus, total):
     assert checkout_solution.checkout(skus) == total
+
